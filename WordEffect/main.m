4251d881b9f42805fd3b4e4df67681a4ac873614
Tyler Boudreau <boudreaut@hotmail.com>
2018-04-09 19:05:26 -0700

    Added part 2 of the assignment

--- WordEffect/main.m
+++ WordEffect/main.m
@@ -11,16 +11,56 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         char inputNum[6]={'1','2','3','4','5','6'};
-        
+        char inputString[255];
         while(TRUE){
             printf("Enter a number from 1-6\n");
+            printf("Enter a string\n");
             
-            fgets(inputNum, 1, stdin);
+            fgets(inputNum,6,stdin);
+            fgets(inputString, 255, stdin);
             printf("Your number is %s\n",inputNum);
+            printf("Your String is %s\n)",inputString);
             
-            NSString *inputString = [NSString stringWithUTF8String:inputNum];
+            NSString *convertNum = [NSString stringWithUTF8String:inputNum];
+            NSString *convertString =[NSString stringWithUTF8String:inputString];
             
-            NSLog(@"Input was :%@",inputString);
+            NSString *loudString = [convertString uppercaseString];
+            NSString *quietString =[convertString lowercaseString];
+            NSUInteger stringLength = [convertString length];
+            NSString *ehString =[convertString stringByAppendingString:@",eh?"];
+            NSString *spaceString =[convertString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+            
+        
+            
+            switch ([convertNum integerValue])
+            {
+                case 1:
+                    NSLog(@"%@",loudString);break;
+                case 2:
+                    NSLog(@"%@",quietString);break;
+                case 3:
+                    NSLog(@"%lu",(unsigned long)stringLength);break;
+                case 4:
+                    NSLog(@"%@",ehString);break;
+                case 5: {
+                    
+                    unichar lastChar = [convertString characterAtIndex:[convertString length] - 2];
+                                     
+                    if (lastChar == '?'){
+                        printf("I don't know \n");
+                    }
+                    else if (lastChar == '!'){
+                        printf("Whoa calm down!\n");
+                    }
+                    else {
+                        NSLog(@"%@",convertString);break;
+                }
+                case 6:
+                    NSLog(@"%@",spaceString);break;
+                
+             }
+            
+        }
         }
     }
     return 0;
